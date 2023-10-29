4c277c9940287aae059f7634b12e9d47717a16c3
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-10-29 21:11:10 +0200

    pycodestyle api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,11 +21,10 @@ def status():
 @app_views.route('/stats')
 def stats():
     """retrieves the number of each object by type"""
-    return jsonify({
-  "amenities": storage.count(Amenity),
-  "cities": storage.count(City),
-  "places": storage.count(Place),
-  "reviews": storage.count(Review),
-  "states": storage.count(State),
-  "users": storage.count(User)
-})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)
+                    })
